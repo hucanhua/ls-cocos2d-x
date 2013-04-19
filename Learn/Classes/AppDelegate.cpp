b58b9d481eecec30096089f5aba4253a0fcf0e89
leafsoar <kltwjt@gmail.com>
2013-04-20 02:52:07 +0800

    local

---
+++ Learn/Classes/AppDelegate.cpp
@@ -0,0 +1,68 @@
+#include "AppDelegate.h"
+
+#include "cocos2d.h"
+#include "controller.h"
+#include "GlobalSchedule/GlobalSchedule.h"
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate()
+{
+
+}
+
+AppDelegate::~AppDelegate()
+{
+
+}
+
+bool AppDelegate::applicationDidFinishLaunching()
+{
+    // initialize director
+    CCDirector *pDirector = CCDirector::sharedDirector();
+    pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
+
+    // turn on display FPS
+    pDirector->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    pDirector->setAnimationInterval(1.0 / 60);
+
+    // create a scene. it's an autorelease object
+//    CCScene *pScene = HelloWorld::scene();
+
+    // run
+//    pDirector->runWithScene(pScene);
+
+    CCScene * pScene = CCScene::create();
+    CCLayer * pLayer = new TestController();
+    pLayer->autorelease();
+
+    pScene->addChild(pLayer);
+    pDirector->runWithScene(pScene);
+
+    GlobalSchedule::start(0.2f);
+//    GlobalSchedule::pause();
+//    GlobalSchedule::resume();
+
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground()
+{
+    CCDirector::sharedDirector()->pause();
+
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground()
+{
+    CCDirector::sharedDirector()->resume();
+    
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+}
---
+++ Learn/Classes/tests.h
@@ -0,0 +1,15 @@
+#ifndef _TESTS_H_
+#define _TESTS_H_
+
+//#include "ActionsTest/ActionsTest.h"
+#include "TouchScaleTest/TouchScaleTest.h"
+
+enum {
+	TEST_TOUCH_SCALE = 0,
+	TESTS_COUNT,
+};
+
+const std::string g_aTestNames[TESTS_COUNT] = {
+		"TouchScale", };
+
+#endif
